4e0623243fbbfa6f737e90627698763f90b02f67
wataoxp <168703313+wataoxp@users.noreply.github.com>
2026-02-25 20:55:37 +0900

    update

--- Src/delay.cpp
+++ Src/delay.cpp
@@ -38,32 +38,4 @@ void tDelay::Delay(uint32_t nTime)
 	}
 }
 
-// nopループを使う方
-#if 0
-uint32_t tDelay::SetClockSource(CoreClock source)
-{
-	if(!source)
-	{
-		return 1;
-	}
-	ClockSource = source;
-
-	return 0;
-}
-
-/* 64MHzの場合、1秒間に64Mサイクル(1us辺り64サイクル)
- * 1ループを4サイクル(命令)と仮定して、1us辺り16サイクル*ループ数とする
- * 64MHz、-Ofast最適化で10usを設定すると20usになった。遅い分には問題ないと見ている
-*/
-void tDelay::uDelay(uint32_t nTime)
-{
-	__IO uint32_t loop = nTime * (ClockSource / 4);
-
-	while(loop--)
-	{
-		__NOP();
-	}
-}
-#endif
-
 
